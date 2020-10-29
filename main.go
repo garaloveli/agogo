69bf74780531c1516436a0237a1ad501251cb760
Olga Zegarra <garaloveli@gmail.com>
2020-10-28 23:13:55 -0500

    CodeChanges

--- main.go
+++ main.go
@@ -4,14 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title:"Test Article", Desc:"Test Description", Content:"Hello World"},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
 func homePage (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage Endpoiint Hit")
+	fmt.Fprint(w, "Homepage Endpoint Hit")
 }
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
